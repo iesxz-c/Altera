ed97210f4dad5ef0f14b3b6f2d0fef0d918409c3
Akash <akash.krishnan.046@gmail.com>
2024-11-21 14:21:29 +0530

    Added console log statement to Layout component.

--- Client/Altera/src/components/Layout.jsx
+++ Client/Altera/src/components/Layout.jsx
@@ -2,7 +2,7 @@ import { Box, Heading, VStack } from '@chakra-ui/react';
 
 const Layout = ({ children }) => (
   <Box p={5}>
-    
+    console.log('Rendering Layout component');
     <VStack spacing={5}>{children}</VStack>
   </Box>
 );
